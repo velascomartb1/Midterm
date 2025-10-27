2b8673a7ce0c78bf32f62e41c30f1a2e7b5f09d3
velascomartb1 <velascomartb1@montclair.edu>
2025-10-27 00:23:22 -0400

    Midterm Project

---
+++ Midterm1/src/main/java/com/example/Midterm1/Midterm1Application.java
@@ -0,0 +1,13 @@
+package com.example.Midterm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Midterm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Midterm1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Midterm1/Midterm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Midterm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Midterm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
